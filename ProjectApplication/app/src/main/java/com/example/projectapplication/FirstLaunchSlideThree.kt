3981a85e62dc6adffb6fd6a664d8374bc8036ea7
Almost Developer <49747572+DavidkaKotlin@users.noreply.github.com>
2019-05-31 10:43:12 +0300

    Деус вульт
    Сделал активность с инфой о тренировке при нажатии

--- ProjectApplication/app/src/main/java/com/example/projectapplication/FirstLaunchSlideThree.kt
+++ ProjectApplication/app/src/main/java/com/example/projectapplication/FirstLaunchSlideThree.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.NumberPicker
+import android.widget.TextView
 import com.kevalpatel2106.rulerpicker.RulerValuePicker
 import com.kevalpatel2106.rulerpicker.RulerValuePickerListener
 import kotlinx.android.synthetic.main.fragment_first_launch_slide_three.*
@@ -21,8 +22,30 @@ class FirstLaunchSlideThree : Fragment() {
         val v = inflater.inflate(R.layout.fragment_first_launch_slide_three, container, false)
         val heightPicker : RulerValuePicker = v.findViewById(R.id.enter_height)
         val weightPicker : RulerValuePicker = v.findViewById(R.id.enter_weight)
+        val heightPickedText : TextView = v.findViewById(R.id.picked_height_text)
+        val weightPickedText : TextView = v.findViewById(R.id.picked_weight_text)
         heightPicker.selectValue(170)
         weightPicker.selectValue(70)
+        heightPicker.setValuePickerListener(object : RulerValuePickerListener{
+            override fun onValueChange(selectedValue: Int) {
+
+            }
+
+            override fun onIntermediateValueChange(selectedValue: Int) {
+                heightPickedText.text = selectedValue.toString()
+            }
+
+        })
+        weightPicker.setValuePickerListener(object : RulerValuePickerListener{
+            override fun onValueChange(selectedValue: Int) {
+
+            }
+
+            override fun onIntermediateValueChange(selectedValue: Int) {
+                weightPickedText.text = selectedValue.toString()
+            }
+
+        })
         return v
     }
 
--- ProjectApplication/app/src/main/java/com/example/projectapplication/TrainingInfoActivity.kt
+++ ProjectApplication/app/src/main/java/com/example/projectapplication/TrainingInfoActivity.kt
@@ -7,16 +7,52 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.ScrollView
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_training_info.*
 
 class TrainingInfoActivity : AppCompatActivity() {
 
+    var exercisesArray = arrayOf("Приседания", "Отжимания", "Подтягивания", "Жим 100кг", "Хз чето еще")
+    var exercisesCountArray = arrayOf(50,23,423,13,53)
+    var exercisesCount = 5
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_training_info)
+        val titleText = findViewById<TextView>(R.id.workout_info_title_text)
+        titleText.text = "День ${intent.getStringExtra("text")}"
+        for(i in 0 until exercisesCount){
+            addItemToLayout(i)
+        }
         info_begin_workout_btn.setOnClickListener {
             startActivity(Intent(this,WorkoutActivity::class.java))
             finish()
         }
+
         }
+
+    private fun addItemToLayout(current: Int) {
+        val mainLayout = findViewById<LinearLayout>(R.id.ex_info_list)
+        val layoutToAdd = LinearLayout(applicationContext)
+        val exNameText = TextView(applicationContext)
+        val exRepeatsCount = TextView(applicationContext)
+        var lparams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        val exNameTextParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT)
+        val exRepeatsCountParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT)
+        lparams.setMargins(15,15,15,15)
+        exNameTextParams.weight = 4f
+        exRepeatsCountParams.weight = 1f
+        layoutToAdd.layoutParams = lparams
+        exNameText.layoutParams = exNameTextParams
+        exRepeatsCount.layoutParams = exRepeatsCountParams
+        layoutToAdd.orientation = LinearLayout.HORIZONTAL
+        exNameText.text = exercisesArray[current]
+        exRepeatsCount.text = exercisesCountArray[current].toString()
+        exNameText.textSize = 20f
+        exRepeatsCount.textSize = 20f
+        exNameText.typeface = Typeface.create("Franklin",Typeface.BOLD_ITALIC)
+        exRepeatsCount.typeface = Typeface.create("Franklin",Typeface.BOLD_ITALIC)
+        layoutToAdd.addView(exNameText)
+        layoutToAdd.addView(exRepeatsCount)
+        mainLayout.addView(layoutToAdd)
     }
+}
