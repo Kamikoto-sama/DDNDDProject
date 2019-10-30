439b7477b7853933f0daf03c7942a8e04dd2cab6
Kamikoto-sama <you@example.com>
2019-10-30 23:09:05 +0500

    Система диалогов. НЕ ГОТОВА

---
+++ Game/KOALA/Assets/Scripts/DialogManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    public Replica StartReplica;
+    private bool _playerInArea;
+    private bool _dialogStarted;
+    public Text ReplicaText;
+
+    private void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.F) || !_playerInArea || _dialogStarted) return;
+        _dialogStarted = true;
+        DisplayReplica(StartReplica);
+    }
+
+    private void DisplayReplica(Replica replica)
+    {
+        ReplicaText.text = replica.text;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) => _playerInArea = true;
+
+    private void OnTriggerExit2D(Collider2D other) => _playerInArea = false;
+}
