4a54b2e75bd583bd00286a83fe01cd8d857a6d2c
Scindapsus345 <zavnikita345@gmail.com>
2019-11-06 21:42:27 +0500

    NPC Vision and Moving

---
+++ Game/KOALA/Assets/Scripts/NPC/NPCMovingCP.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class NPCMovingCP : MonoBehaviour
+{
+    public float aCoordX;
+    public float bCoordX;
+    public float speed;
+    public float delay;
+    public bool isMoving;
+    public bool isLookingRight;
+    public EntityState state;
+    public Vector3 dir;
+    private NPCVision _vision;
+
+    public enum EntityState
+    {
+        GoA,
+        GoB,
+        Wait
+    }
+
+    void Start()
+    {
+        _vision = GetComponent<NPCVision>();
+        state = EntityState.GoA;
+        if (transform.position.x > aCoordX)
+            dir = Vector3.left;
+        else
+        {
+            dir = Vector3.right;
+        }
+    }
+    
+    void Update()
+    {
+        _vision.isReverse = !isLookingRight;
+        switch (state)
+        {
+            case EntityState.Wait:
+                EntityState nextState;
+                if (Math.Abs(transform.position.x - aCoordX) < Math.Abs(transform.position.x - bCoordX))
+                    nextState = EntityState.GoB;
+                else
+                    nextState = EntityState.GoA;
+                if (isMoving)
+                {
+                    StartCoroutine(Stay(nextState));
+                    isMoving = false;
+                }
+
+                break;
+            case EntityState.GoA:
+                isMoving = true;
+                GoTo(aCoordX);
+                break;
+            case EntityState.GoB:
+                isMoving = true;
+                GoTo(bCoordX);
+                break;
+        }
+            
+    }
+
+    void GoTo(float targetX)
+    {
+        if (transform.position.x > targetX)
+        {
+            isLookingRight = false;
+        }
+        if (transform.position.x < targetX)
+        {
+            isLookingRight = true;
+        }
+        if (Math.Abs(transform.position.x - targetX) < 0.001)
+        {
+            state = EntityState.Wait;
+            return;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(targetX, transform.position.y), speed * Time.deltaTime);
+    }
+
+    IEnumerator Stay(EntityState nextState)
+    {
+        yield return new WaitForSeconds(delay);
+        state = nextState;
+    }
+}
---
+++ Game/KOALA/Assets/Scripts/NPC/NPCVision.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using UnityEngine;
+
+public class NPCVision : MonoBehaviour
+{
+    public string targetTag = "Player";
+    public int rays = 12;
+    public int distance = 15;
+    public float angle = 30;
+    public Vector2 offset;
+    public float rotation;
+    public bool isReverse = false;
+    private GameObject _target;
+
+    void Start()
+    {
+        _target = GameObject.FindGameObjectWithTag(targetTag);
+    }
+
+    public void Reverse()
+    {
+        isReverse = !isReverse;
+    }
+
+    bool GetRaycast(Vector2 dir) 
+    {
+        var result = false;
+        var pos = (Vector2)transform.position + offset;
+        var hit = Physics2D.Raycast(pos, dir, distance);
+        if (hit.collider != null)
+        {
+            if (hit.transform.gameObject == _target)
+            {
+                result = true;
+                Debug.DrawLine((Vector3)pos, hit.point, Color.green);
+            }
+            else
+            {
+                Debug.DrawLine((Vector3)pos, hit.point, Color.blue);
+            }
+        }
+        else
+        {
+            Debug.DrawRay(new Vector3(pos.x, pos.y), new Vector3(dir.x, dir.y) * distance, Color.red);
+        }
+        return result;
+    }
+
+    bool RayToScan()
+    {
+        bool result = false;
+        bool isHit = false;
+        float rayAngle = GetStartAngle();
+        for (int i = 0; i < rays; i++)
+        {
+            var x = Mathf.Cos(rayAngle);
+            var y = Mathf.Sin(rayAngle);
+            
+            if (isReverse)
+                rayAngle -= angle * Mathf.Deg2Rad / (rays - 1);
+            else
+                rayAngle += angle * Mathf.Deg2Rad / (rays - 1);
+
+                Vector2 dir = transform.TransformDirection(new Vector2(x, y));
+            if (GetRaycast(dir)) isHit = true;
+        }
+
+        if (isHit) result = true;
+        return result;
+    }
+
+    private float GetStartAngle()
+    {
+        var rayAngle = rotation * Mathf.Deg2Rad;
+        rayAngle -= angle * Mathf.Deg2Rad / 2;
+        if (isReverse)
+            rayAngle = Mathf.PI - rayAngle;
+        return rayAngle;
+    }
+
+    void Update()
+    {
+        if (Math.Abs(transform.position.x - _target.transform.position.x) < distance)
+        {
+            if (RayToScan())
+            {
+                print("Объект обнаружен!");
+            }
+            else
+            {
+                // Поиск цели...
+            }
+        }
+    }
+}
