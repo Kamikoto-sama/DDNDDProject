970e493a69b1294d51db05898a48c5f37419b887
IAlmostDeveloper <51601917+IAlmostDeveloper@users.noreply.github.com>
2019-11-22 04:22:27 +0300

    added climbing, fixed crouching

---
+++ Game/KOALA/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float runSpeed;
+    public float climbSpeed;
+    float horizontalMove;
+    float verticalMove;
+    public bool jump = false;
+    public bool crouch = false;
+    public LayerMask whatIsLadder;
+    public LayerMask whatIsGround;
+    public float distance;
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if (Input.GetButtonDown("Jump"))
+            jump = true;
+        if (Input.GetButtonDown("Crouch"))
+            crouch = true;
+        else if (Input.GetButtonUp("Crouch"))
+            crouch = false;
+        CheckForLadder();
+    }
+
+    private void CheckForLadder()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.up, distance, whatIsLadder);
+        if (hitInfo.collider == null) return;
+        verticalMove = Input.GetAxisRaw("Vertical") * climbSpeed;
+        controller.Climb(verticalMove, jump);
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
