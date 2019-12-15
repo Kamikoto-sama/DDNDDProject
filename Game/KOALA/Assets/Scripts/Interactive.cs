a6450e5c1b4c5d8241b2e4160bb54993624e86aa
Kamikoto-sama <you@example.com>
2019-12-15 17:09:43 +0500

    Add Interactive class

---
+++ Game/KOALA/Assets/Scripts/Interactive.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactive : MonoBehaviour
+{
+    protected bool playerInArea;
+    protected readonly HashSet<string> triggersTags;
+
+    public Interactive() =>
+        triggersTags = new HashSet<string>(new[]
+        {
+            "Player"
+        });
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (triggersTags.Contains(other.tag))
+            playerInArea = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (triggersTags.Contains(other.tag))
+            playerInArea = false;
+    }
+}
