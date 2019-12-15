684e1ae7fef60eb5e18cff98a37f31d14d44ba86
Kamikoto-sama <you@example.com>
2019-12-15 17:08:44 +0500

    Subtitles fixes

---
+++ Game/KOALA/Assets/Scripts/Dialogs/Subtitles.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Dialogs
+{
+    public class Subtitles : MonoBehaviour
+    {
+        public Text TextField;
+        public Replica[] Replicas;
+
+        public void Init() => StartCoroutine(PrintReplicas());
+
+        private IEnumerator PrintReplicas()
+        {
+            foreach (var replica in Replicas)
+            {
+                TextField.text = replica.Text;
+                yield return new WaitForSeconds(replica.Time);
+            }
+        }
+    }
+
+    [Serializable]
+    public class Replica
+    {
+        public float Time = 2;
+        [TextArea(1, 10)]
+        public string Text;
+    }
+}
