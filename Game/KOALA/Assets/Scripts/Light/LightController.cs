e16cf2ba6f029246ad2a9b0bda8fa9536b005272
Kamikoto-sama <you@example.com>
2019-12-15 17:10:05 +0500

    MultiLightSwitch

---
+++ Game/KOALA/Assets/Scripts/Light/LightController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+
+namespace Light
+{
+	public class LightController : Interactive
+	{
+		private Light2D lightComponent;
+		public bool HasPower { get; set; } = true;
+		private bool lightState = true;
+
+		private void Start() => lightComponent = GetComponent<Light2D>();
+
+		private void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.F) && playerInArea)
+				lightState = !lightState;
+			lightComponent.enabled = HasPower && lightState;
+		}
+	}
+}
---
+++ Game/KOALA/Assets/Scripts/Light/MultiLightController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Light
+{
+    public class MultiLightController : Interactive
+    {
+        public GameObject[] LightSources;
+        private IEnumerable<LightController> lightControllers;
+        private bool powerState = true;
+
+        private void Start()
+        {
+            PrepareLightSources();
+        }
+
+        private void PrepareLightSources()
+        {
+            lightControllers = LightSources.Select(ls =>
+            {
+                var lightController = ls.GetComponent<LightController>();
+                if (lightController == null)
+                    throw new ArgumentException("At least one light object hasn't a LightController");
+                return lightController;
+            });
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F) && playerInArea)
+                SwitchPower();
+        }
+
+        private void SwitchPower()
+        {
+            powerState = !powerState;
+            foreach (var lightController in lightControllers)
+                lightController.HasPower = powerState;
+        }
+    }
+}
