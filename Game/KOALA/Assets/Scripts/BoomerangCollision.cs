240a8a5c61388f17c06ac61cfa6926e291fd93f1
Scindapsus345 <zavnikita345@gmail.com>
2020-01-08 17:57:11 +0500

    Boomerang v1.1 (F)

--- Game/KOALA/Assets/Scripts/BoomerangCollision.cs
+++ Game/KOALA/Assets/Scripts/BoomerangCollision.cs
@@ -7,13 +7,14 @@ public class BoomerangCollision : MonoBehaviour
 {
     public Rigidbody2D rigidbody2D;
 
-    void Start()
-    {
-        rigidbody2D = GetComponent<Rigidbody2D>();
-    }
+//    void Start()
+//    {
+//        rigidbody2D = GetComponent<Rigidbody2D>();
+//    }
 
     void OnCollisionEnter2D(Collision2D other)
     {
+        rigidbody2D = GetComponent<Rigidbody2D>();
         if (/*other.gameObject.CompareTag("Ground") && */!Boomerang.BoomIsComingBack)
             rigidbody2D.bodyType = RigidbodyType2D.Static;
     }
