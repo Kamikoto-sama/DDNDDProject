86fbd525929d07841c7fe1705b651fbd468c77a2
IAlmostDeveloper <51601917+IAlmostDeveloper@users.noreply.github.com>
2020-01-22 06:08:13 +0500

    added respawn point

---
+++ Game/KOALA/Assets/Scripts/DeathPoint.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DeathPoint : MonoBehaviour
+{
+    [SerializeField] private GameObject respawn;
+    [SerializeField] private GameObject player;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+        var position = respawn.transform.position;
+        player.transform.position = new Vector2(position.x, position.y);
+    }
+}
